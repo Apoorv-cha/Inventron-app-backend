7657d0e0c968b7178c926abf4a0a3827ef5e58fc
Apoorv Chaudhary <81978228+Apoorv-cha@users.noreply.github.com>
2023-02-20 01:54:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://inventron-app.vercel.app"],
     credentials: true,
   })
 );
